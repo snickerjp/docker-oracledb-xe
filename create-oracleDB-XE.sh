b5f571d51ebaf9f5457102c8835b35960eaec2f2
atom <snickerjp>
2022-07-21 12:10:13 +0000

    update create-oracleDB-XE.sh

--- create-oracleDB-XE.sh
+++ create-oracleDB-XE.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-
-# require
-# Download oracle-xe-11.2.0-1.0.x86_64.rpm.zip from Oracle site.
-# into -> docker-images/OracleDatabase/SingleInstance/dockerfiles/11.2.0.2/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+#!/bin/env bash
 
 cd docker-images/OracleDatabase/SingleInstance/dockerfiles
-# help buildDockerImage.sh -h
-# for Oracle 11.2.0.2 Express Edition
-sh buildDockerImage.sh -v 11.2.0.2 -x
+# help buildContainerImage.sh -h
+# for Oracle 21.3.0 Express Edition
+bash buildContainerImage.sh -x -v 21.3.0 -o '--build-arg SLIMMING=false'
